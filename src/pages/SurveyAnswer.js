579821761d3c27a771e4fca6cc1a0a1b5f266a67
BuildTools <unconfigured@null.spigotmc.org>
2023-05-31 18:54:41 +0200

    load

--- src/pages/SurveyAnswer.js
+++ src/pages/SurveyAnswer.js
@@ -1,16 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useSelector} from 'react-redux';
-import {ButtonSurveySelect} from '../actions/ButtonSurveySelect';
+import { useSelector, useDispatch} from 'react-redux';
+import { SurveyFetchAsync } from 'actions/LoadSurveyDataAsync';
+import { useEffect } from 'react';
 import QuestionAnswerTable from '../components/QuestionAnswerTable';
 import {UserTable} from 'components/UserTable';
 
 export default function SurveyEditor() {
   const surveys = useSelector(state => state.surveys);
   const currentUser = {id: "2d9dc5ca-a4a2-11ed-b9df-0242ac120003",email: "julia.newbie@world.com"} //2d9dc868-a4a2-11ed-b9df-0242ac120003   2d9dc5ca-a4a2-11ed-b9df-0242ac120003
-
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(SurveyFetchAsync())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className = "form-group">
-      <ButtonSurveySelect />
+     
       <table className='table' > 
 
       <thead>  
