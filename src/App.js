ce15d9785a7756bd62c2897598b35e76a2cbe9f5
Tobiáš Koza <Tobi Koza@LAPTOP-62UBQE1R>
2023-06-14 17:02:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     
     {/* Pages for different tasks, Editing survey, answering to assigned survey, results from answers */}
     <SurveyEditor /> 
-    {/* <SurveyAnswer /> */}
+    <SurveyAnswer />
     {/* <SurveyResult />       */}
       
     </div>
