f2b25a1ba9e5d69033f19aff21360845da3453f8
Tobiáš Koza <Tobi Koza@LAPTOP-62UBQE1R>
2023-06-15 16:05:47 +0200

    TommyBroken

--- src/async/AnswerExpiredFetch.js
+++ src/async/AnswerExpiredFetch.js
@@ -1,33 +1,29 @@
 import { updateAnswerExpired } from 'features/SurveySlice';
-import {AnswerUpdateExpiredMutation} from '../queries/AnswerUpdateExpiredMutation';
+import { AnswerUpdateExpiredMutation } from '../queries/AnswerUpdateExpiredMutation';
 
-
-
+// Action creator function
 export const AnswerExpiredFetch = (props) => (dispatch, getState) => {
-  
+  // Iterate through each question in the props
   props.questions.forEach((question) => {
+    // Iterate through each answer in the question
     question.answers.forEach((answer) => {
 
-      AnswerUpdateExpiredMutation({id: answer.id, lastchange: answer.lastchange, expired: true})
+      // Call the AnswerUpdateExpiredMutation function with answer details
+      AnswerUpdateExpiredMutation({ id: answer.id, lastchange: answer.lastchange, expired: true })
         .then(response => response.json())
         .then(json => {
           // Extract the projects data from the JSON response
           const newAnswer = json.data?.answerUpdate.answer;
-          
+
+          // If a new answer is received from the server, dispatch the updateAnswerExpired action
           if (newAnswer) {
-            dispatch(updateAnswerExpired({answer: newAnswer}));
-            console.log('Answer closed on server')
+            dispatch(updateAnswerExpired({ answer: newAnswer }));
+            console.log('Answer closed on server');
           }
-    
-          return json
-        })  
+
+          return json;
+        });
 
     });
   });
-
-
- 
-
-
-
-  };
+};
