96131b6b3f2b123e32c2bec319ef3eac038fc1c1
BuildTools <unconfigured@null.spigotmc.org>
2023-06-16 06:52:29 +0200

    COmmit

--- src/components/ShowAnswersTable.js
+++ src/components/ShowAnswersTable.js
@@ -2,34 +2,30 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /*
-Counts the occurrences of answer values in the given questions.
+Counts the occurrences of answer values in the given question.
 */
-function ShowAnswersTable(props) {
-  const countAnswerValues = (questions) => {
-    const answerCounts = {};
+function countAnswerValues(question) {
+  const answerCounts = {};
 
-    questions.forEach((question) => {
-      if (question.type.name === 'Uzavřené' || question.type.name === 'Škála') {
-        question.answers.forEach((answer) => {
-          const answerValue = answer.value;
+  if (question.type.name === 'Uzavřené' || question.type.name === 'Škála') {
+    question.answers.forEach((answer) => {
+      const answerValue = answer.value;
 
-          if (answerValue in answerCounts) {
-            answerCounts[answerValue] += 1;
-          } else {
-            answerCounts[answerValue] = 1;
-          }
-        });
+      if (answerValue in answerCounts) {
+        answerCounts[answerValue] += 1;
+      } else {
+        answerCounts[answerValue] = 1;
       }
     });
+  }
 
-    return answerCounts;
-  };
-
-  const answerCounts = countAnswerValues(props.questions);
+  return answerCounts;
+}
 
 /*
 Renders a table to display question answers and their counts.
 */
+function ShowAnswersTable(props) {
   return (
     <table className="table">
       <thead>
@@ -45,29 +41,19 @@ Renders a table to display question answers and their counts.
             const uniqueAnswers = Array.from(
               new Set(question.answers.map((answer) => answer.value))
             );
+            const answerCounts = countAnswerValues(question); // Calculate answer counts for the current question
 
-            /*
-            Renders a table row for a question with its unique answers and counts
-            */
-            return (
-              <tr key={question.id}>
-                <td>{question.name}</td>
-                <td>
-                  {uniqueAnswers.map((answerValue) => (
-                    <div key={`${question.id}-${answerValue}`}>
-                      {answerValue}
-                    </div>
-                  ))}
-                </td>
-                <td>
-                  {uniqueAnswers.map((answerValue) => (
-                    <div key={`${question.id}-${answerValue}`}>
-                      {answerCounts[answerValue] || 0}
-                    </div>
-                  ))}
-                </td>
+            // Render a table row for each unique answer
+            return uniqueAnswers.map((answerValue, index) => (
+              <tr key={`${question.id}-${answerValue}`}>
+                {/* Only render the question name for the first unique answer */}
+                {index === 0 && (
+                  <td rowSpan={uniqueAnswers.length}>{question.name}</td>
+                )}
+                <td>{answerValue}</td>
+                <td>{answerCounts[answerValue] || 0}</td>
               </tr>
-            );
+            ));
           }
 
           return null;
