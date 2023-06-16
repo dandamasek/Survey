ec69f8644f73178ab45839dd6bebc5a247e54eaa
BuildTools <unconfigured@null.spigotmc.org>
2023-06-16 07:37:33 +0200

    Com

--- src/components/ShowAnswersTable.js
+++ src/components/ShowAnswersTable.js
@@ -8,13 +8,16 @@ function countAnswerValues(question) {
   const answerCounts = {};
 
   if (question.type.name === 'Uzavřené' || question.type.name === 'Škála') {
+    question.values.forEach((value) => {
+      answerCounts[value.name] = 0; // Initialize answer count for each value
+      console.log(value.name)
+    });
+
     question.answers.forEach((answer) => {
       const answerValue = answer.value;
 
       if (answerValue in answerCounts) {
         answerCounts[answerValue] += 1;
-      } else {
-        answerCounts[answerValue] = 1;
       }
     });
   }
@@ -22,38 +25,38 @@ function countAnswerValues(question) {
   return answerCounts;
 }
 
+
 /*
-Renders a table to display question answers and their counts.
+Renders a table to display question values and their counts.
 */
-function ShowAnswersTable(props) {
+function ShowValuesTable(props) {
+  const { questions } = props;
+
   return (
     <table className="table">
       <thead>
         <tr>
           <th>Question</th>
-          <th>Answer</th>
+          <th>Value</th>
           <th>Count</th>
         </tr>
       </thead>
       <tbody>
-        {props.questions.map((question) => {
+        {questions.map((question) => {
           if (question.type.name === 'Uzavřené' || question.type.name === 'Škála') {
-            const uniqueAnswers = Array.from(
-              new Set(question.answers.map((answer) => answer.value))
-            );
             const answerCounts = countAnswerValues(question); // Calculate answer counts for the current question
 
-            // Render a table row for each unique answer
-            return uniqueAnswers.map((answerValue, index) => (
-              <tr key={`${question.id}-${answerValue}`}>
-                {/* Only render the question name for the first unique answer */}
-                {index === 0 && (
-                  <td rowSpan={uniqueAnswers.length}>{question.name}</td>
-                )}
-                <td>{answerValue}</td>
-                <td>{answerCounts[answerValue] || 0}</td>
-              </tr>
-            ));
+            return (
+              <React.Fragment key={question.id}>
+                {question.values.map((value) => (
+                  <tr key={`${question.id}-${value.id}`}>
+                    <td>{question.name}</td>
+                    <td>{value.name}</td>
+                    <td>{answerCounts[value.name] || 0}</td>
+                  </tr>
+                ))}
+              </React.Fragment>
+            );
           }
 
           return null;
@@ -63,4 +66,4 @@ function ShowAnswersTable(props) {
   );
 }
 
-export default ShowAnswersTable;
+export default ShowValuesTable;
