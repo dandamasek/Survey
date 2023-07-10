82ab320da6ea5d492ccc0c137f7cd1f23568faf7
dandamasek <masekdaniel02@gmail.com>
2023-07-10 15:54:26 +0200

    Update: fixing previous actions

--- src/actions/AnswerShowButton.js
+++ src/actions/AnswerShowButton.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ShowAnswerTable from '../components/ShowAnswersTable';
+
 /**
  * Component for displaying a dropdown menu of surveys and showing the selected survey's answers.
  * @param {object} props - The component props containing the necessary details.
  * @returns {JSX.Element} - The rendered component.
  * @function 
  */
+
 const AnswerShowButton = (props) => {
   const [selectedOption, setSelectedOption] = useState('');
 
